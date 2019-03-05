6de8b85fc9a8d61181c8216e412998732b9501cd
Jason Lei <jlei@absolute.com>
2019-03-05 15:43:04 -0800

    .

--- absolute/conftest.py
+++ absolute/conftest.py
@@ -1,10 +1,12 @@
+import os
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from absolute.actions.login_actions import LoginActions
 from simple_settings import settings
 
 
-@pytest.fixture
+@pytest.yield_fixture(scope="function", autouse=True)
 def fixture_webdriver() -> webdriver:
     driver = webdriver.Chrome()
     driver.maximize_window()
--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
 def pytest_addoption(parser):
     parser.addoption('--settings', action='store')
-
+    # parser.addoption("--driver", action="store", default="chrome", help="Type in browser type (e.g. chrome)")
+    # parser.addoption("--headless", action="store", default="headless", help="Is headless driver?")
